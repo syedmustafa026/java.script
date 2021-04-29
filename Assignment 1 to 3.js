5facbb8182d321b42d4014dbab890cecaf74d670
Syed Mustafa Ahmed <77160234+syedmustafa026@users.noreply.github.com>
2021-04-29 23:43:17 +0500

    alert

---
+++ Assignment 1 to 3.js
@@ -0,0 +1,22 @@
+alert("Hello Mr. Bean.\nHow are you?")
+
+var title = "Hello World"
+
+alert(title)
+
+var age = 18
+
+alert (age)
+
+var book = "A smarter way to learn Javascript"
+
+document.write(book)
+
+var birthYear = "\n2003"
+
+document.write(birthYear)
+
+
+
+
+
