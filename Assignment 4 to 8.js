dbc744547b8cacdb73bf09e3ef35a4c180b84987
Syed Mustafa Ahmed <77160234+syedmustafa026@users.noreply.github.com>
2021-04-30 00:20:55 +0500

    mathematical expressions

---
+++ Assignment 4 to 8.js
@@ -0,0 +1,323 @@
+//                        ASSIGNMENTS OF CHAPTER 4 to 8 //
+//                              QUESTION 1
+
+// var a = "myPhonicBook"
+// b = "iLoveMyMom"
+// c = littleCuteBook
+
+
+
+//                               QUESTION 2
+//  LEGAL:
+//  var userFriendlyWebsite
+//  var ahmed_25
+//  var blueWater
+//  var $45D
+//  var mustafaAhmed_026
+
+
+// ILLEGAL:
+// var 25mustafa
+// var black case
+// var bring-black
+// coffee 25
+// mythpath 65
+
+
+// ASSIGNMENT OF CHAPTER 4
+//     QUESTION 3
+
+// var Rules_For_Naming_js_variables = 
+// "<h1> Rules for naming JS variables </h1> <p> Variable names can only contain numbers, $, . and - For example $my_1stVariable<br> Variables must Begin with a letter, $ or . For example $name, _name or name<br>Variable names are case sensitive<br>Variable names should not be JS keywords<br></p>"
+// document.write (Rules_For_Naming_js_variables) 
+
+//                                ASSIGNMENT OF CHAP 5
+//                                    QUESTION 1 & 2
+
+// var a = 3
+// b = 5 
+// c = 100
+
+// document.write("Sum of 3 + 5 is ")
+// document.write(a + b ) 
+
+
+// document.write("<br/> Sum of 3 * 5 is ")
+// document.write(a * b)
+
+
+// document.write("<br/> Sum of 3 - 5 is ")
+// document.write(a - b)
+
+
+// document.write("<br/> Sum of 3 / 5  is ")
+// document.write(a / b)
+
+// document.write("<br/> % of 3 & 5  is ")
+// document.write(a / b * c)
+
+//                                   QUESTION 3
+
+// document.write("Value after variable declaration is = a")
+// var a = 5
+// b = 6
+// c = 7 
+// document.write("<br/>Initial Value = " )
+// document.write(a)
+// document.write(" <br/>Value after increment is " )  
+// document.write(a + b)    
+// document.write("<br/> Value after addition is ")
+// document.write(a + b + c   )
+// document.write("<br/> Value after decrement is ")
+// document.write( c - b )
+// document.write("<br/> The remainder is ")
+// document.write(b/a)
+
+
+//                                       Question 4
+
+// document.write("Cost of one movie ticket is 600 PKR. Write a script tostoreticket price in a variable & calculate the cost of buying 5 tickets to a movie.")
+
+// var ticket = 600
+
+// document.write("<br/><br/>Total cost of buying 5 tickets is ")
+// document.write(ticket * 5)
+
+//                                            Question 5
+
+
+// var a = 4
+// document.write("Table of 4 <br/>")
+// document.write( "4 x 1 = " )
+// document.write( a * 1)
+// document.write( "<br/>4 x 2 = " )
+// document.write( a * 2)
+// document.write( "<br/>4 x 3 = " )
+// document.write( a * 3)
+// document.write( "<br/>4 x 4 = " )
+// document.write( a * 4)
+// document.write( "<br/>4 x 5 = " )
+// document.write( a * 5)
+// document.write( "<br/>4 x 6 = " )
+// document.write( a * 6)
+// document.write( "<br/>4 x 7 = " )
+// document.write( a * 7)
+// document.write( "<br/>4 x 8 = " )
+// document.write( a * 8)
+// document.write( "<br/>4 x 9 = " )
+// document.write( a * 9)
+// document.write( "<br/>4 x 10 = " )
+// document.write( a * 10)
+// document.write( "<br/>4 x 11 = " )
+// document.write( a * 11)
+
+
+
+//                                       Question 6
+
+
+// var C = 25
+
+// var F = 85
+// document.write("25C is ")
+// document.write( C = (F -32)*5/9)
+
+// document.write("<br/>45F is ")
+// document.write( F = C* 9/5+32 )
+
+
+
+//                                     Question 7
+
+// document.write("<h1>Shopping Cart</h1><br/>")
+
+// var item1 = 500
+// var item2 = 1000
+
+// document.write("<br/>Price of item 1 is <br/>")
+// document.write( item1)
+// document.write("<br/>Ordered quantity of item 1 is <br/>")
+// document.write( 1+1)
+// document.write("<br/>Price of item 2<br/>")
+// document.write( item2 )
+// document.write("<br/>Ordered Quantity of item 2 is <br/>")
+// document.write( 2+1 )
+// document.write('<br/>Shipping charges is<br/>')
+// document.write( 200 )
+// document.write( "<h2><br/>Total bill is <br/>" )
+// document.write( item1 * 2 + item2 * 3) 
+
+
+//                                       Question 8
+
+
+// var total = 850
+// obtained = 659 
+
+// document.write("<h1>Mark sheet</h1>")
+
+// document.write("<br/>Total Marks : ")
+// document.write( total )
+// document.write("<br/>Obtained Marks : ")
+// document.write( obtained)
+// document.write("<br/>Percentage : ")
+// document.write( obtained/total*100)
+
+
+
+//                                      Question 9
+
+// var $ = 104.80
+// sr = 28
+
+// document.write("<h1> Currency in PKR</h1>")
+
+// document.write("<h3>Total Currency in PKR: </h1>")
+
+// document.write( $ * 10 + sr * 25)
+
+
+//                                   Question 10
+
+// var a = 20
+
+// alert( a +5 *10 /2 )
+
+
+
+//                                     Question 11
+
+// var current = 2021
+// birth = 2003
+
+// document.write("<br/>current year : ")
+// document.write( current)
+// document.write("<br/>birth year : ")
+// document.write( birth)
+// document.write("<br/>your age : ")
+// document.write( current - birth)
+
+
+
+//                              Question 12
+
+
+// var pi = 3.142
+// r = 5
+
+// document.write("<h1>The Geometizer</h1>")
+// document.write("<br/>Radius of circle: ")
+// document.write( r)
+// document.write("<br/>The circumference is: ")
+// document.write( 2*pi*r)
+// document.write("<br/> The Area is: ")
+// document.write( pi*r*r)
+
+
+//                                      Question 13
+
+// var fav = ("Lays wavy")
+// age = 18
+// max = 70
+// estimate = 3
+
+// document.write("<h1>The Life time supplier</h1>")
+// document.write("<br/>Favorite snack: ")
+// document.write( fav)
+// document.write("<br/>Estimated maximum age: ")
+// document.write( max)
+// document.write("<br/>Amount of snack per day: ")
+// document.write(estimate)
+// document.write("<br/>You will need  ")
+// document.write(  3 *max) 
+// document.write( " to last you until the ripe old age of ")
+// document.write( max )
+
+
+
+
+
+//                    ASSIGNMENT OF CHAPTER 6-9
+//                           Question 1
+
+// var a = 10
+// document.write("Result <br/> ")
+// document.write("The value of "+ (a)+ "<hr/>")
+
+
+// document.write("The value of ++a is: " +(++a)+"<br/>")
+// document.write("Now the value of a is: " +(a)+"<br/><br/>")
+
+// document.write("The value of a++ is: " +(a++)+"<br/>")
+// document.write("Now the value of a is: " +(a)+"<br/><br/>")
+
+// document.write("The value of --a is: " +(--a)+"<br/>")
+// document.write("Now the value of a is: " +(a)+"<br/><br/>")
+
+// document.write("The value of a-- is: " +(a--)+"<br/>")
+// document.write("Now the value of a is: " +(a)+"<br/>")
+
+
+
+//                           Question 2
+
+// var a = 2
+// b = 1
+
+// var result = --a - --b + ++b + b--
+// document.write( result )
+
+// Expaination;
+// --a
+//   1  
+// --a - --b 
+//   1 -   0  
+// --a - --b + ++b
+//   1 -   0     1  
+// --a - --b + ++b + b--
+//   1 -   0  +  1 +  1  =  3
+
+//                              Question 3
+
+//  var message = "Welcome, ";
+//  var userName = prompt("Your Name");
+//  var banger = "!";
+//  var customMess = message + userName + banger;
+//  alert(customMess)
+
+
+//                                Question 5
+
+// var table = prompt("Enter any Table");
+// var length = 10;
+// var i = 1;
+
+// document.write("<h1>Multiplication table: "+ table+ "</h1>");
+// for(i=1; i <= length; ++i )
+//  document.write("<br>"+table+" * "+i+" = " +(table*i));
+//                                 Question 6
+
+
+// var totalMarksSub1 = 100
+// totalMarksSub2 = 100
+// totalMarksSub3 = 100
+
+
+// Sub1 = 68
+// Sub2 = 89
+// Sub3 = 92
+// result1 = (Sub1/totalMarksSub1) *100
+// result2 = (Sub2/totalMarksSub2) *100
+// result3 = (Sub3/totalMarksSub3) *100
+// totalPercentage = (Sub1 + Sub2 + Sub3)/300 * 100
+// document.write("<h1>Subject  Total marks  Obtained Marks  Percentage<br/>")
+// document.write( "<br/>English &nbsp;&nbsp;100 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 68 <br/>")
+// document.write( "<br/>maths &nbsp;&nbsp;100 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 57 <br/>")
+// document.write( "<br/>urdu  &nbsp;&nbsp;100 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 44 <br/><br/>")
+// document.write( " &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ")
+// document.write(totalMarksSub1+totalMarksSub2+totalMarksSub3)
+// document.write( " &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ")
+// document.write(result1+result2+result3)
+// document.write( " &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ")
+// document.write( " &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ")
+// document.write(totalPercentage)
