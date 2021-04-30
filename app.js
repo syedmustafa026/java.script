8b0d7b7b45c2710a55c1191b0b1895246bd8da96
Syed Mustafa Ahmed <77160234+syedmustafa026@users.noreply.github.com>
2021-05-01 00:18:40 +0500

    using if statement

---
+++ app.js
@@ -0,0 +1,228 @@
+//                          ASSIGNMENT OF CHAPTER 9 TO 11
+//                                      Question no. 1
+
+
+//  var name = prompt( "Enter your City Name" )
+//  city1 = ("karachi")
+//  city2 = ("lahore")
+//  city3 = ("islamabad")
+//  if (city1 === name ){
+//      alert("“Welcome to city of lights! Taste its Biryani”" )
+//  }
+//  if (city2 === name ){
+//     alert("“laft right phr right phr seedha”" )
+// }
+// if (city3 === name ){
+//     alert("“Welcome to city of Borgers”" )
+// }
+
+
+//                                      Question no. 2
+
+//  var gender = prompt("Whats your Gender?")
+
+//  gender1 = ("male") 
+//  gender2 = ("female")
+
+//  if (gender === gender1){
+//      alert("Hello Sir!")
+//  }
+
+//  if (gender === gender2 ){
+//     alert("Hello madam!")
+// }
+// else if(gender !== gender2 && gender !== gender1 ){
+// alert( "You are neither male nor female you are other person!")}
+
+//                                      Question no. 3
+
+
+//  var light = prompt("What is the color of traffic light")
+//  color1 = ("red")
+//  color2 = ("yellow")
+//  color3 = ("green")
+//  if (light === color1){
+//      alert("MUST STOP")
+//  }
+//  if (light === color2){
+//     alert("Ready to move")
+// }
+// if (light === color3){
+//     alert("Move Now")
+// }
+
+
+
+//                                      Question no. 4
+
+// var fuel = +prompt( "How much fuels remaining in your car(In ltrs.)")
+
+// if (fuel > 0.26){
+//     alert("“Enjoy your ride with driving safety”")
+//     }
+// if (fuel <= 0.25){
+// alert("“Please refill the fuel in your car”")
+// }
+
+
+//                                      Question no. 5
+
+//  var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// // It Will runs
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// It Will runs
+
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// It Will runs
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+// It Will runs
+
+//  if (true){
+// alert("True");
+// }
+// // It Will runs
+
+// if (false){
+// alert("False");
+// }
+// if("car" < "cat"){
+//     alert("car is smaller than cat");
+//     }
+// It Will runs
+
+
+//                                      Question no. 6
+
+// var Marks = +prompt("Write your marks obtained.")
+// fullMarks = 300
+
+
+// if (Marks >= 250 && Marks <= 300) {
+// document.write( "<h1>Mark Sheet</h1><br/>"+ "<h3>TOTAL MARKS = " + fullMarks +"<br/> Marks obtained = " + Marks + "<br/>Percentage = "+ Marks/fullMarks*100 +"<br/> Grade = A-one <br/>Remarks = Excellent! " )
+// }
+// else if (Marks >= 231 && Marks <=249) {
+//     document.write( "<h1>Mark Sheet</h1><br/>"+ "<h3>TOTAL MARKS = " + fullMarks +"<br/> Marks obtained = " + Marks + "<br/>Percentage = "+ Marks/fullMarks*100 +"<br/> Grade = A <br/>Remarks = Good "  )}
+
+// else if (Marks >= 180 && Marks <=230)  {
+//     document.write( "<h1>Mark Sheet</h1><br/>"+ "<h3>TOTAL MARKS = " + fullMarks +"<br/> Marks obtained = " + Marks + "<br/>Percentage = "+ Marks/fullMarks*100 +"<br/> Grade = B<br/>Remarks = You need to improve"  )}
+
+// else if (Marks >= 0 && Marks <=179) { 
+//     document.write( "<h1>Mark Sheet</h1><br/>"+ "<h3>TOTAL MARKS = " + fullMarks +"<br/> Marks obtained = " + Marks + "<br/>Percentage = "+ Marks/fullMarks*100 +"<br/> Grade = Fail <br/>Remarks = Sorry"  )}
+
+
+//                                      Question no. 7
+
+// var number = +prompt("Guess any number from 1 to 10")
+// a = 7
+// b = 6
+// c = 8
+// if (number === a) {
+//     alert("Bingo! you are correct")
+// }
+// else if (number === b) {
+//     alert("“Close enough to the correct answer”.")
+// }
+// else if (number === c) {
+//     alert("“Close enough to the correct answer”.")
+// }
+// else {
+//     alert("try again")
+// }
+
+//                                      Question no. 8
+
+// var num = prompt("Write any number to check if is divisible by 3" )
+// if (num % 3 > 0 ){
+//     alert(num +" not divisible by 3")
+// }
+// else{
+//     alert( " is divisible by 3")
+// }
+
+//                                      Question no. 9
+
+// var num = prompt("Enter the number to check that it is even number or an odd number.")
+
+// if (num % 2){
+//     alert("This is odd number")
+// }
+// else {
+//     alert("This is even number")
+// }
+
+
+//                                      Question no. 10
+
+// var T= prompt("Enter your city temperature? in C")
+
+// if (T > 40){
+//     alert("“It is too hot outside.”")
+// }
+// else if (T > 30){
+//     alert("“The Weather today is Normal.”")
+// }
+
+// else if (T > 20){
+//     alert("“Today’s Weather is cool.”")
+// }
+
+// else if (T > 10){
+//     alert("“OMG! Today’s weather is so Cool.”")
+// }
+
+//                                         Question no. 11
+ //                                            CALCULATOR
+// var first = +prompt("Enter first number")
+// var second = prompt("Choose Operation (+, -, *, /, %)")
+// var third = +prompt("Enter second number")
+
+// if (second === "+"){
+//     alert(first + third)
+// }
+// else if (second === "-"){
+//     alert(first - third)
+// }
+// else if (second === "*"){
+//     alert(first * third)
+// }
+// else if (second === "/"){
+//     alert(first / third)
+// }
+// else if (second === "%"){
+//     alert(first * 100 / third )
+// }
+
+
+
+
+
+
+
+
+
+
+
