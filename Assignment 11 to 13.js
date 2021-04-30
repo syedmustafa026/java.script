855075115fb6dfb120b5813f6e8f5017700908c2
Syed Mustafa Ahmed <77160234+syedmustafa026@users.noreply.github.com>
2021-05-01 00:31:39 +0500

    using if else statement

---
+++ Assignment 11 to 13.js
@@ -0,0 +1,91 @@
+//                              ASSIGNMENT OF CHAPTER 12 and 13
+//                                      Question no. 2
+
+// var num = prompt("Enter any first Integer number")
+// num2 = prompt("Enter any second Integer number")
+
+// if (num > num2 ){
+//     alert(num + " is greater than "+ num2 )
+// }
+// else if(num2 > num) {
+//     alert(num2 + " is greater than "+ num )
+// }
+
+//                                      Question no. 3
+
+// var num = +prompt("Enter any number")
+
+// if (num !== "-"-num || num === num){
+//     alert(num+" is positive")
+// }
+// else if (num === 0){
+//     alert("Number is zero")
+
+// }
+// else {
+//     alert(num +"is negative number")
+// }
+
+
+//                                      Question no. 4
+
+// var vowel = prompt("Enter any alphabet")
+
+// if (vowel === "a"|| vowel === "e"|| vowel === "i"|| vowel === "o"|| vowel === "u"){
+//     alert("It is vowel")
+// }
+// else {
+//     alert("It is not vowel")
+// }
+
+
+//                                      Question no. 5
+
+
+// var first = prompt("Enter your email")
+// sec = prompt("Enter your password")
+// third = prompt("type again your password")
+
+
+// if ( third ===sec){
+//     alert("“Correct! The password you entered matches the original password”.")
+// }
+// else if ( third !== sec ){
+//     alert("Password dosent match")
+// }
+// else {
+//     alert("Enter your password")
+// }
+
+
+//                                      Question no. 6
+//  ********************* FIXED*********************
+// var greeting = prompt("Enter time hour")
+// if (greeting < 18) {
+// alert(greeting = "Good day")}
+// else 
+// {alert(greeting = "Good evening")}
+
+
+
+//                                      Question no. 7
+// var time = prompt("Enter time like 1200 means 12:00")
+//  if (time >=0000 && time <1200){
+//      alert("Good morning")
+//  }
+// else if (time >=1200 && time <1700){
+//     alert("Good afternoon")
+// }
+// else if (time >=1700 && time <2100){
+//     alert("Good evening")
+// }
+//  else if (time >=2100 && time <2359){
+//     alert("Good night")
+// }
+// else {
+//     alert("please enter correct time method")
+// }
+
+
+
+
--- app2.js
+++
@@ -1,3 +0,0 @@
-    //                              ASSIGNMENT OF CHAPTER 12 and 13
-     //                                      Question no. 1
-
