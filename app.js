17c0432471b83164cc096f8e704f51f670fb8787
Syed Mustafa Ahmed <77160234+syedmustafa026@users.noreply.github.com>
2021-05-02 23:16:44 +0500

    using arrays

---
+++ app.js
@@ -0,0 +1,159 @@
+//                                Assignment 14 to 17 
+//                                    Question 1
+// student names un array
+// var students =[ "syed", "mustafa", "ahmed", "ghous", "tayyab", "arif", "babar", "shadab", "kamran", "azam", "imaam", "faisal"]
+
+//                                    Question 2
+
+//creating empty array
+// var arr = []
+
+//                                    Question 3
+
+// var arr1 =["ALLAH SWT"]
+
+//                                    Question 4
+
+// var num = [1,2,3,4]
+
+
+//                                    Question 5
+
+// var boolean = [true , false]
+
+//                                    Question 6
+
+// var mixed_array = ["This is", true , 24+"times"]
+
+//                                    Question 7
+
+// var list = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M", "Phil.", "PhD"]
+
+// document.write("<h3>Qualification</h3>")
+// document.write("<ol>", "<li>"+list[0]+"</li>","<li>"+list[1]+"</li>","<li>"+list[2]+"</li>","<li>"+list[3]+"</li>","<li>"+list[4]+"</li>","<li>"+list[5]+"</li>","<li>"+list[6]+"</li>","<li>"+list[7]+"</li>","<li>"+list[8]+"</li>","</ol>")
+
+
+//                                    Question 8
+
+// var nam = ["Micheal", "John", "Tony"]
+// num = [320,230,480]
+// total = 500
+// percetage = [num[0]/total*100, num[1]/total*100 , num[2]/total*100]
+
+// document.write("Score of "+ nam[0]+ " is and percentage is " + percetage[0]+ "%<br/>")
+// document.write("Score of "+ nam[1]+ " is and percentage is " + percetage[1]+ "%<br/>")
+// document.write("Score of "+ nam[2]+ " is and percentage is " + percetage[2]+ "%<br/>")
+
+//                                    Question 9
+// part a & b
+
+// var input = prompt( "what color do you choice?")
+// input1 = prompt( "where to put this color(start or end)")
+// colors =["red","yellow","green"]
+// if ( input1 === "starting"){
+// colors.unshift(input)
+// alert(colors)}
+
+//part c to e
+
+// input3 = prompt("from where to remove this color(start or end)")
+// colors =["red","yellow","green"]
+
+// if (input3=== "start"){
+//  colors.shift()
+//  alert(colors)
+// }
+// if (input3=== "end"){
+//     colors.pop()
+//  alert(colors)
+
+// }
+
+//part f to g
+
+
+// var type = prompt( "There is only three colors"+'\n'+"(red yellow and green)" +'\n'+"Wanna remove or add")
+// var colorNames = prompt( "color name")
+//  index = prompt( "at which point")
+
+// color =["red","yellow","green"]
+// if (type === "add"){
+// color.splice( index, 0, colorNames)
+// alert (color)
+// }
+// else if(type === "remove"){
+// if (colorNames ==="red"){
+//     color.splice(0,1)
+//     alert(color)
+// }
+// else if (colorNames ==="yellow"){
+//     color.splice(1,1)
+//     alert(color)
+// }
+// else if (colorNames ==="green"){
+//     color.splice(2,2)
+//     alert(color)}
+// }
+
+
+
+//                                    Question 10
+
+// var score = [656,654,664,645]
+// var scores = [656,654,664,645]
+
+// scores.sort()
+
+// document.write("<h2>Score of students = "+[score]+"</h1>")
+// document.write("<h3>Ordered Score of students = "+scores.sort()+"</h1>")
+
+
+//                                    Question 11
+
+
+// var  cities =["karachi","lahore","peshawar","islamabad","multan",]
+// document.write("<h4>Cities list: "+cities+"</h1>")
+// var selectedCities = cities.slice(2,4 )
+// document.write("<h5>Selected Cities list:  "+selectedCities+"</h1>")
+
+
+//   //                                 Question 12
+
+// var arr2 = ["This ", " is ", " my ", " cat"];
+// var arr = ["This ", " is ", " my ", " cat"];
+
+
+
+// document.write("<h2>Array; "+arr2+"<br/>")
+// document.write("Strings; "+arr.join(''))
+
+
+//                                   __Qustion 13 __
+
+
+// let arr= ["keyboard","mouse","printer","monitor"]
+
+// console.log(arr[0])
+// console.log(arr[1])
+// console.log(arr[2])
+// console.log(arr[3])
+
+//                                    __Qustion 14__
+
+// let arr= ["keyboard","mouse","printer","monitor"]
+
+// console.log(arr[3])
+// console.log(arr[2])
+// console.log(arr[1])
+// console.log(arr[0])
+
+
+
+
+
+
+
+
+
+
+
