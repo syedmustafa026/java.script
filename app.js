c003283d8ac6f5582028a94d5cbdb64d118ad9c9
Syed Mustafa Ahmed <77160234+syedmustafa026@users.noreply.github.com>
2021-05-06 23:13:23 +0500

    js file

---
+++ app.js
@@ -0,0 +1,133 @@
+//                           Assignment 17 to 20
+//                                Question 1
+// multidimesions empty 3
+// var data = [[], [], []];
+
+//                                Question 2
+
+// for (let i = 0; i < 5; i++) {
+
+    // for (let j = 0; j < 2; j++) {
+    //     console.log(i,j)
+    // }
+// }
+
+//                                Question 3
+
+// for (let i = 0; i <= 10; i++) {
+//    console.log(i)
+
+// }
+
+
+//                                Question 4
+// var table = prompt("enter table number")
+// length = prompt("How many times")
+
+// for( i = 1; i <= length; i++){
+//     console.log(table + "*" +i+ "="+ table.length * i  )
+// }
+
+//                                Question 5
+
+// fruits = ["apple", "banana", "mango", "orange","strawberry"]
+
+// for (var i = 0; i < fruits.length; i++) {
+//      document.write("<h3>"+fruits[i] );
+// }
+
+
+//                                Question 6
+
+// counting loop
+
+// document.write("<h1>Counting</h1><br/> ")
+// for (let i = 0; i <=15; i++) {
+//     document.write(+[i]+ ",")
+// }
+// document.write("<h1>Reverse Counting</h1><br/> ")
+// for (let i = 10; i >= 0; i--) {
+//     document.write([i]+ ",")
+// }
+// document.write("<h1>Even</h1><br/> ")
+// num = []
+
+// var num = [];
+// for (var i = 0; i <= 20; i += 2) {
+//     num.push(i);
+// }
+// document.write(num)
+
+
+// document.write("<h1>Odd</h1><br/> ")
+// var number = [];
+// for (var i = 1; i <= 19; i += 2) {
+//     number.push(i);
+// }
+// document.write(number)
+
+// document.write("<h1>Series</h1><br/> ")
+// var ser = [];
+// for (var i = 2; i <= 20; i += 2) {
+//     ser.push(i+"k");
+// }
+// document.write(ser)
+
+
+
+//                                Question 7
+
+
+// input = prompt("Welcome to our bakery what you want?")
+
+// if(input === products){
+//     alert("This product is available at" + products+ "index")
+// }
+
+
+// products = ["cake", "apple pie", "cookie", "chips", "patties"]
+// input = prompt("Welcome to our bakery what you want?")
+// var milGaya = 'nahi';
+
+// for (var i = 0; i < products.length; i++) {
+//     console.log(i)
+//     if (input === products[i]) {
+//         milGaya = 'han';
+//         alert("It's available at " + i + " index");
+       
+//     }
+//     else if  (milGaya === 'nahi') {
+//        alert("It's not available");
+//        break;
+//    }
+// }
+
+//                                  Question 8
+
+// var a = [24, 53, 78, 91, 12]
+// document.write("<h3> Number List: "+a+"</h3>")
+// newnum = a.sort()
+// final = newnum.reverse()
+// document.write("Largest number: " +final[0])
+
+//                                  Question 9
+
+// var a = [24, 53, 78, 91, 12]
+// document.write("<h3> Number List: "+a+"</h3>")
+// newnum = a.sort()
+// document.write("Smallest number: " +newnum[0])
+
+
+
+//                                  Question 10
+
+// num = []
+// for (let i = 5; i <= 100; i+=5) {
+// console.log(i)
+// }
+
+
+
+
+
+
